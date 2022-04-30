ad05d9aedf1ef321be720ec2180ec2cf4c0d5106
Rakib544 <md.rakib10122003@gmail.com>
2022-04-30 14:30:13 +0600

    blog created functionality complete

--- app/routes/models/post.server.ts
+++ app/routes/models/post.server.ts
@@ -16,9 +16,10 @@
 //   ];
 // }
 
+import type { Post } from "@prisma/client";
 import { prisma } from "~/db.server";
 
-export type { Post } from "@prisma/client";
+export type { Post };
 
 export async function getPosts() {
   return prisma.post.findMany();
@@ -26,4 +27,10 @@ export async function getPosts() {
 
 export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug } });
+}
+
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({ data: post });
 }
